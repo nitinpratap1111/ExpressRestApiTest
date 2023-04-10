c55916a598b52cdd90f7f84e89c9f121e844ec1d
nitinpratap1111 <you@example.comprpratap.nitin11@gmail.com>
2023-04-10 16:51:35 +0530

    add features of post or create user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const app = express()
+const uuid = require('uuid')
 
 
 const members = [
@@ -24,6 +25,8 @@ const members = [
 
 ]
 
+app.use(express.json())
+
 app.get("/showAllUser",(req,res)=>{
     res.status(200).json(members)
 })
@@ -35,7 +38,19 @@ app.get("/showUser/:uid",(req,res)=>{
     
 })
 
+app.post("/addUser/",(req,res)=>{
+    //console.log(req.body)
+    
+    // const name = req.body.name
+    // const email = req.body.email
+    // const password = req.body.password
+    const { name,email,password} = req.body
+    //console.log(name,email,password)
+    members.push({id:uuid.v4(),name,email})
+    res.status(200).json(members)
+})
 
 
-const PORT= 3002;
+
+const PORT= process.env.PORT || 3002;
 app.listen(PORT,()=>console.log(`express is running at ${PORT}`))
